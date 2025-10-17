3e6496a5f58f4d6d1f2909723cffc9a8890fb56e
Rudy V <eagleeye1689@gmail.com>
2025-10-16 19:41:29 -0500

    Added Spring Boot properly

---
+++ java-spring-boot-back-end-app/calling-card/src/main/java/com/example/calling_card/CallingCardApplication.java
@@ -0,0 +1,13 @@
+package com.example.calling_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallingCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallingCardApplication.class, args);
+	}
+
+}
