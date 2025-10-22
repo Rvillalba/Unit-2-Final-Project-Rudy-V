d9df961a6e58108376bd0638de5bfdf907165696
Rudy V <eagleeye1689@gmail.com>
2025-10-21 20:11:42 -0500

    Added controller for the users table

---
+++ java-spring-boot-back-end-app/calling-card/src/main/java/com/example/calling_card/controllers/UsersController.java
@@ -0,0 +1,44 @@
+package com.example.calling_card.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @GetMapping ("/all")
+    public List<Users> getAllUsers() {
+        return usersRepository.findAll();
+    }
+
+    @GetMapping ("{id}")
+    public Users getUserById(@PathVariable int id) {
+        return usersRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping ("/add")
+    public Users addUser(@RequestBody Users user) {
+        return usersRepository.save(user);
+    }
+
+    @PutMapping("/update/{id}")
+    public Users updateUser(@PathVariable int id, @RequestBody Users userDetails) {
+        Users user = usersRepository.findById(id).orElse(null);
+        if (user != null) {
+            user.setName(userDetails.getName());
+            user.setEmail(userDetails.getEmail());
+            user.setPhoneNumber(userDetails.getPhoneNumber());
+            return usersRepository.save(user);
+        }
+        return null;
+    }
+
+    @DeleteMapping ("/delete/{id}")
+    public void deleteUser(@PathVariable int id) {
+        usersRepository.deleteById(id);
+    }
+}
