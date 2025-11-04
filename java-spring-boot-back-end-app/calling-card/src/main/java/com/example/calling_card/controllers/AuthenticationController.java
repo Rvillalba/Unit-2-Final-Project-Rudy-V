a1629e8eb66e648ffc453878949f4ec5d865496b
eagleeye1689@gmail.com <rudyv@stltrades.com>
2025-11-04 12:06:07 -0600

    Refactored code and fixed minor errors

--- java-spring-boot-back-end-app/calling-card/src/main/java/com/example/calling_card/controllers/AuthenticationController.java
+++ java-spring-boot-back-end-app/calling-card/src/main/java/com/example/calling_card/controllers/AuthenticationController.java
@@ -2,7 +2,10 @@ package com.example.calling_card.controllers;
 
 import com.example.calling_card.dto.JwtAuthenticationResponse;
 import com.example.calling_card.dto.SignUpRequest;
+import com.example.calling_card.services.AuthenticationService;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +18,14 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/signup")
-    public JwtAuthenticationResponse signup(@RequestBody SignUpRequest request)
+    public JwtAuthenticationResponse signup(@RequestBody SignUpRequest request) {
         return authenticationService.signup(request);
+    }
 
     @PostMapping("/signin")
-    public JwtAuthenticationResponse signin(RequestBody SignInRequest request)
+    public JwtAuthenticationResponse signin(RequestBody SignInRequest request) {
         return authenticationService.signin(request);
+    }
 
 
 }
