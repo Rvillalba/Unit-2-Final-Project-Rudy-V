07147c217f94eedb291d8c464def2bdf16b0e4f4
Rudy V <eagleeye1689@gmail.com>
2025-11-03 23:37:53 -0600

    Created new services and added Jwt code

--- java-spring-boot-back-end-app/calling-card/src/main/java/com/example/calling_card/config/SecurityConfig.java
+++ java-spring-boot-back-end-app/calling-card/src/main/java/com/example/calling_card/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.calling_card.config;
 
+import com.example.calling_card.filters.JwtAuthenticationFilter;
+import com.example.calling_card.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ java-spring-boot-back-end-app/calling-card/src/main/java/com/example/calling_card/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.example.calling_card.filters;
+
+import com.example.calling_card.services.JwtService;
+import com.example.calling_card.services.UserService;
+import io.micrometer.common.util.StringUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) {
+        throw ServletException,IOException {
+            final String authHeader = request.getHeader("Authorization");
+            final String jwt;
+            final String userEmail;
+            if (StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader, "Bearer ")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            jwt = authHeader.substring(7);
+            logger.debug("JWT - {}", jwt.toString());
+            userEmail = jwtService.extractUsername(jwt);
+            if (StringUtils.isNotEmpty(userEmail)) && SecurityContextHolder.getContext().getAuthentication() == null)
+
+                var userDetails = userService.userDetailsService().loadUserByUsername(userEmail);
+                if (jwtService.isTokenValid(jwt, userDetails)) {
+                    log.debug("User - {}", userDetails);
+                    SecurityContext context = SecurityContextHolder.createEmptyContext();
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities()
+                    );
+                    authToken.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(request)
+                    );
+                    context.setAuthentication(authToken);
+                    SecurityContextHolder.setContext(context);
+                }
+            }
+
+            filterChain.doFilter(request, response);
+    }
+
+}
