8e726bbf7160bb1c0d053b9cbeb257bfc13e30a3
Rudy V <eagleeye1689@gmail.com>
2025-11-06 19:12:37 -0600

    Added SavedCardRequest DTO

---
+++ java-spring-boot-back-end-app/calling-card/src/main/java/com/example/calling_card/dto/SavedCardRequest.java
@@ -0,0 +1,9 @@
+package com.example.calling_card.dto;
+
+public class SavedCardRequest {
+    public int userId;
+    public int cardId;
+    public String name;
+    public String email;
+    public String phoneNumber;
+}
