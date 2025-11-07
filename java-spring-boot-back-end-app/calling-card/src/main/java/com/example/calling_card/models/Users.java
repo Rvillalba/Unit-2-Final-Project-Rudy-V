605b8a4badcf15a8a2b5da17d9a740c237e62a65
Rudy V <eagleeye1689@gmail.com>
2025-11-06 18:46:19 -0600

    Added one to many relationship to Users

--- java-spring-boot-back-end-app/calling-card/src/main/java/com/example/calling_card/models/Users.java
+++ java-spring-boot-back-end-app/calling-card/src/main/java/com/example/calling_card/models/Users.java
@@ -1,9 +1,9 @@
 package com.example.calling_card.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Users {
@@ -15,32 +15,53 @@ public class Users {
     private String email;
     private String phoneNumber;
 
-    public Users(){
-            }
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<SavedCards> savedCards = new ArrayList<>();
+
+    public Users() {
+    }
+
     // Getters and Setters
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
 
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPhoneNumber() {
         return phoneNumber;
     }
+
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
+    public List<SavedCards> getSavedCards() {
+        return savedCards;
+    }
+
+    public void setSavedCards(List<SavedCards> savedCards) {
+        this.savedCards = savedCards;
+    }
+
+
 }
